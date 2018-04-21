598cbaf9399d9c58a155741566624fecff2c2674
matias <mati.carballo91@gmail.com>
2018-04-21 22:24:08 +0200

    Algo funcando online
    Bueno a probar con marcoti

---
+++ Assets/PlayerConnectionObject.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerConnectionObject : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (!isLocalPlayer)
+        {
+            //por ejemplo pintarlo azul
+            return;
+        }
+        CmdSpawnMyUnit();
+
+    }
+
+    public GameObject PlayerUnitPrefab;
+
+    [SyncVar]
+    public string PlayerName = "Anonymus cumbiero";
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            CmdSpawnMyUnit();
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            CmdChangePlayerName("Mati " + Random.Range(1, 100));
+        }
+
+    }
+    [Command]
+    void CmdSpawnMyUnit()
+    {
+        GameObject go = Instantiate(PlayerUnitPrefab);
+        NetworkServer.SpawnWithClientAuthority(go,connectionToClient);
+    }
+    [Command]
+    void CmdChangePlayerName(string nombre)
+    {
+        PlayerName = nombre;
+    }
+
+    /*
+    [ClientRpc]
+    void RpcChangePlayerName(string nombre)
+    {
+        PlayerName = nombre;
+    }
+    */
+}
---
+++ Assets/PlayerUnit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerUnit : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!hasAuthority)
+        {
+            return;
+        }
+        if(Input.GetKeyDown(KeyCode.Space) )
+        {
+            this.transform.Translate(0, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
