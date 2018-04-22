2f09990e87c15b030ef10f32c231f04743ed4e5c
Leandro <leandrosuarez0@gmail.com>
2018-04-22 16:04:07 -0300

    Se agregó el salto
    En el script PlayerMovement se agregó variables int jumpSpeed, bool onGround y se modificaron condificiones de if para que eno se pueda volver a saltar hasta que toques el suelo

--- Assets/PlayerGoodies/PlayerMovement.cs
+++ Assets/PlayerGoodies/PlayerMovement.cs
@@ -8,8 +8,10 @@ public class PlayerMovement : NetworkBehaviour {
     Vector3 velocity = new Vector3(0.0f, 1.0f, 0.0f);
     float floorHeight = 0.0f;
     float sleepThreshold = 0.05f;
+    public int jumpSpeed = 3;
     public float gravity = -9.8f;
     public float alpha = 1;
+    bool onGround;
 
     void Start()
     {
@@ -17,15 +19,17 @@ public class PlayerMovement : NetworkBehaviour {
     }
     
     void FixedUpdate()
-    { 
+    {
         if (velocity.magnitude > sleepThreshold || transform.position.y > floorHeight)
         {
+            onGround = false;
             velocity += new Vector3(0.0f, gravity * Time.fixedDeltaTime, 0.0f);
         }
 
         transform.position += velocity * Time.fixedDeltaTime;
         if (transform.position.y <= floorHeight)
         {
+            onGround = true;
             transform.position = new Vector3(transform.position.x, floorHeight, transform.position.z);
             velocity.y = 0;
         }
@@ -40,9 +44,9 @@ public class PlayerMovement : NetworkBehaviour {
         }
         int vel_x = 0;
         int vel_y = 0;
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && onGround)
         {
-            vel_y = 1;
+            vel_y = jumpSpeed;
         }
         if (Input.GetKey(KeyCode.A))
         {
@@ -56,9 +60,9 @@ public class PlayerMovement : NetworkBehaviour {
         {
             vel_x = 1;
         }
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
         {
-            vel_y = 1;
+            vel_y = jumpSpeed;
         }
         
         velocity = new Vector3(vel_x, vel_y, 0.0f);
